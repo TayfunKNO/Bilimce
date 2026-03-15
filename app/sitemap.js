410b43d3c3458e868d514e5540ae29aaf082b8ed
TayfunKNO <tayfunkucuknurioglu@gmail.com>
2026-03-15 16:03:41 +0300

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -10,7 +10,25 @@ export default function sitemap() {
       url: 'https://bilimce.vercel.app/auth',
       lastModified: new Date(),
       changeFrequency: 'monthly',
+      priority: 0.3,
+    },
+    {
+      url: 'https://bilimce.vercel.app/favorites',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
       priority: 0.5,
+    },
+    {
+      url: 'https://bilimce.vercel.app/reading-list',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://bilimce.vercel.app/profile',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.4,
     },
   ]
 }
